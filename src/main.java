5b13a14081a0ea4ff4bba8595fcf840ae840edab
vinsifroid <duchene113@gmail.com>
2015-10-07 19:20:27 +0200

    Add Option Menu and fixe some bug

--- src/main.java
+++ src/main.java
@@ -13,163 +13,215 @@
  * import java.lang.*; 
  */
 
-public class main
+
+public class Main
 {
-<<<<<<< HEAD
+	public static int vies = 7;
+	public static byte joueurs = 1;
+		
 	public static void main (String [] args)
 	{
-		/* 	
-		 * Déclaration des variables
-		 */
+		  /* 
+		   * ==========================
+		   *    	LE PENDU
+		   * ==========================
+		   * 
+		   * game to play "le pendu". Here is the main class were all the
+		   * program is executed
+		   * 
+		   *There are actually 6 functions:
+		   * 
+		   * 1) main
+		   * 2) jeu where the game is played
+		   * 3) replaceCharAt subroutines of the game
+		   * 4) menu
+		   * 5) option1 to decide the number of lifes
+		   * 6) option2 to decide the number of players
+		   */
 		
-		/*	
-		 * ==========================
-		 * 			LE PENDU
-		 * ==========================
-		 * 
-		 * jeu destiné à faire un pendu. Ici il s'agit de la classa mère où
-		 * tout se déroulera 
-		 * 
-		 * Le programme se sépare actuellement en 3 fonctions
-		 * 
-		 * 1) main
-		 * 2) jeu où se déroule le jeu
-		 * 3) menu
-		 */
 				menu();				
 		
 	}
-public static void jeu(int vies, byte joueurs)
-{
-	// all the variables used
-	String MotSecret = "zigote";	
-	String MotUser = "";
-	String LettresFausses = "";
-	char LettreUser = 'A';	
-	int LongueurMot = MotSecret.length();
-	boolean re = true, same = false;	
-				
-	if (joueurs == 2)
-	{//loop to define the word
-	      //ask the first player to enter a word
-	}
-	else
+	 /*
+	  * subroutine to play the game
+	  * @pre : number of lifes (n>0) and number of players (1 or 2)
+	  * @post: play the game
+	  */
+	public static void jeu()
 	{
-	      //Take a word in a file
+		// all the variables used
+		String MotSecret = "zigote";	
+		String MotUser = "";
+		String LettresFausses = "";
+		char LettreUser = 'A';	
+		int LongueurMot = MotSecret.length();
+		boolean re = true, same = false;	
+					
+		if (joueurs == 2)
+		{//loop to define the word
+		      //ask the first player to enter a word
+		}
+		else
+		{
+		      //Take a word in a file
+		}
+		/*
+		 * 	Texte dédié Interface utilisateur
+		 */
+		for (byte o = 0; o < LongueurMot; o++) // loop to have the same number of * than the letters in the "MotSecret"
+		  {
+		    MotUser = MotUser + "*";
+		  }
+		System.out.println("Trouvez le mot secret !");	
+		
+		while( vies != 0)
+		{
+			
+			System.out.println(String.format("\n\n\nIl reste : %d vie(s)",(vies)));
+			System.out.println(MotUser);
+			System.out.println("Vous avez déjà proposé les lettres suivantes: " + LettresFausses);
+						
+			System.out.println("Ecrivez une lettre : ");
+			same = false;
+			re = true;
+			
+			
+			LettreUser = TextIO.getChar();
+	
+			for(byte i=0; i < LongueurMot; i++) //loop to test all the string
+			{	
+				if (LettreUser == MotSecret.charAt(i)) //if it's ok => fonction to replace the chain
+				{				
+					MotUser = replaceCharAt(MotUser, LettreUser, i);
+					re = false;
+				//	changeChar(MotUser,i,LettreUser);
+				}
+			}
+			for(int l=0; l < LettresFausses.length(); l++)
+			{
+				if(LettreUser == LettresFausses.charAt(l))
+				{
+					same = true;
+				}
+			}
+			
+			if(MotUser.equals(MotSecret))
+			{
+				System.out.println("Félicitation, vous avez trouvé le mot " + MotSecret + " avec encore " + (vies) + " vie(s) !!!");						
+				break;
+			}	
+			
+			if (re && !same) //if the letters was wrong, minus 1 life
+			{
+				vies--;
+				LettresFausses = LettresFausses + LettreUser + " ";
+			}
+			
+			if(vies == 0)
+			{
+				System.out.println("==========================\n\t GAME OVER\n==========================");
+			}
+		}
+		
 	}
 	/*
-	 * 	Texte dédié Interface utilisateur
+	 * @pre -
+	 * @post replace a char in a string in at a certain position
 	 */
-	for (byte o = 0; o < LongueurMot; o++) // loop to have the same number of * than the letters in the "MotSecret"
-	  {
-	    MotUser = MotUser + "*";
-	  }
-	System.out.println("Trouvez le mot secret !");	
-	
-	while( vies != 0)
+	public static String replaceCharAt(String s, char c, int pos)
 	{
+		return s.substring(0,pos) + c + s.substring(pos+1);
+	}
+	/*
+	 * @pre -
+	 * @post give a menu to user and then redirect him
+	 */
+	public static void menu()
+	{
+		byte choix = 0;				
 		
-		System.out.println(String.format("\n\n\nIl reste : %d vie(s)",(vies)));
-		System.out.println(MotUser);
-		System.out.println("Vous avez déjà proposé les lettres suivantes: " + LettresFausses);
-					
-		System.out.println("Ecrivez une lettre : ");
-		same = false;
-		re = true;
-		
-		
-		LettreUser = TextIO.getChar();
-
-		for(byte i=0; i < LongueurMot; i++) //loop to test all the string
-		{	
-			if (LettreUser == MotSecret.charAt(i)) //if it's ok => fonction to replace the chain
-			{				
-				MotUser = replaceCharAt(MotUser, LettreUser, i);
-				re = false;
-			//	changeChar(MotUser,i,LettreUser);
-			}
+		do
+		{
+		System.out.println("==========================\n\t LE PENDU \n==========================");
+		System.out.println("1.Jouer \n2.Options \n3.Crédits \n4.Quitter");
+			
+		choix = TextIO.getByte();
+		switch(choix)
+		{
+			case 1 : 
+					System.out.println("\n C'est parti !");
+					jeu();									
+				break;
+			case 2 :					
+					menuOption();			
+				break;
+			case 3 :
+				System.out.println("Developpement : F.Duchene && L.Tascon Gutierrez \nVersion du pendu 1.0");					
+				break;
+			case 4 :
+				System.out.println("Program ended correctly");
+				break;			
+			default :
+					System.out.println("Veuillez indiquer 1,2,3 ou 4 !");
+				break;
 		}
-		for(int l=0; l < LettresFausses.length(); l++)
+		
+		}while(choix != 4);
+	}
+	//All of the following instructions are about the option menu
+	public static void menuOption()
+	{
+		byte choix = 0;		
+		do
 		{
-			if(LettreUser == LettresFausses.charAt(l))
-			{
-				same = true;
-			}
+			System.out.println("************************\n\tOptions:\n************************");
+			System.out.println("Voulez-vous Changer :\n1. le nombre de vies ?\n2. le nombre de joueurs ?");
+			System.out.println("\n3. Retourner en arrière");
+			choix = TextIO.getByte();
+		
+				switch(choix)
+				{
+				case 1:
+					option1();
+					break;
+				case 2:
+					option2();
+					break;
+				case 3:
+					break;
+				default:
+					System.out.println("Veuillez indiquer 1,2 ou 3 !");
+					break;
+				}
+		}while(choix != 3);
+	}
+	/*
+	 * @pre -
+	 * @post change the value of the member variable vies
+	 */
+	public static void option1()	//subroutines in the options to choose the number of life(42)
+	{	
+		System.out.println("Vous avez actuellement " + vies + " vies");
+		System.out.println("Combien voulez vous de vies ?");
+		vies = TextIO.getInt();		
+		if(vies == 0)
+		{
+			vies = 1;
 		}
-		
-		if(MotUser.equals(MotSecret))
+	}
+	/*
+	 * @pre -
+	 * @post change the value of the member variable joueurs
+	 */
+	public static void option2()
+	{	
+		System.out.println("Vous êtes actuellement en mode " + joueurs + " joueur(s)");
+		System.out.println("1. 1 Player\n2. 2 Players");
+		joueurs = TextIO.getByte();		
+		if(joueurs != 1 || joueurs != 2)
 		{
-			System.out.println("Félicitation, vous avez trouvé le mot " + MotSecret + " avec encore " + (vies) + " vie(s) !!!");						
-			break;
-		}	
-		
-		if (re && !same) //if the letters was wrong, minus 1 life
-		{
-			vies--;
-			LettresFausses = LettresFausses + LettreUser + " ";
+			joueurs = 1;
 		}
 	}
 	
-}
-
-public static String replaceCharAt(String s, char c, int pos)
-{
-	return s.substring(0,pos) + c + s.substring(pos+1);
-}
-
-public static void menu()
-{
-	byte choix = 0;		
-	byte joueurs = 1;			
-	int vies = 7;				
-	
-	do
-	{
-	System.out.println("==========================\n\t LE PENDU \n==========================");
-	System.out.println("1.Jouer \n2.Options \n3.Crédits \n4.Quitter");
-		
-	choix = TextIO.getByte();
-	switch(choix)
-	{
-		case 1 : 
-				System.out.println("\n C'est parti !");
-				jeu(vies, joueurs);									
-			break;
-		case 2 :
-				System.out.println("************************\n\tOptions:\n************************");
-				vies = option1();
-				joueurs = option2();
-				jeu(vies, joueurs);					
-			break;
-		case 3 :
-			System.out.println("Developpement : F.Duchene && L.Tascon Gutierrez \nVersion du pendu 1.0");					
-			break;
-		case 4 :
-			System.out.println("Program ended correctly");
-			break;			
-		default :
-				System.out.println("Veuillez indiquer 1,2,3 ou 4 !");
-			break;
-	}
-	
-	}while(choix != 4);
-}
-
-public static int option1()	//subroutines in the options to choose the number of life
-{
-	int vies = 7;
-	System.out.println("Combien voulez vous de vies ?");
-	vies = TextIO.getInt();
-	return vies;
->>>>>>> d0bad613a85b754d31f19c348bc61a32611ab7d3
-}
-
-public static byte option2(){
-    byte joueurs = 1;
-    System.out.println("1. 1 Player\n2. 2 Players");
-    joueurs = TextIO.getByte();
-    if (joueurs == 1){return 1;}
-    else {return 2;}
-}
- 
 }
