8e6896b6b2ebf40515dafa554712379ecc9b4f63
Bivisi <brutusltg@gmail.com>
2016-01-13 16:55:34 +0100

    correct some bug

--- src/TabAligne3.java
+++ src/TabAligne3.java
@@ -4,24 +4,24 @@
  */
 public class TabAligne3
 {
-	private class Aligne3
-	{
-		public int x = -1;
-		public int y = -1;
-		public boolean joueur = false;// alignement ordi == false, allignement joueur == true
-		public int type = 0; // 1 -> ligne, 2 -> colonne, 3 -> diagonale descendante, 4 -> diagonale montante
+    private class Aligne3
+    {
+        public int x = -1;
+        public int y = -1;
+        public boolean joueur = false;// alignement ordi == false, allignement joueur == true
+        public int type = 0; // 1 -> ligne, 2 -> colonne, 3 -> diagonale descendante, 4 -> diagonale montante
 
-		/**
-		 * Constructor for objects of class Aligne3
-		 */
-		public Aligne3(int x, int y, boolean joueur, int type )
-		{
-			this.x = x;
-			this.y = y;
-			this.joueur = joueur;
-			this.type = type;
-		}
-	}
+        /**
+         * Constructor for objects of class Aligne3
+         */
+        public Aligne3(int x, int y, boolean joueur, int type )
+        {
+            this.x = x;
+            this.y = y;
+            this.joueur = joueur;
+            this.type = type;
+        }
+    }
 
     private Aligne3[] tab;
     private int difficulte;
@@ -265,6 +265,14 @@ public class TabAligne3
         return -1;
     }
 
+    public void viderTab()
+    {
+        for(int i = 0;i < tab.length; i++)
+        {
+            tab[i] = null;
+        }
+    }
+    
     private void ajouterA(int x, int y, boolean joueur, int type)
     {
         for(int i = 0; i < tab.length; i++)
@@ -316,17 +324,6 @@ public class TabAligne3
         }
         return true; // vide
     }
-
-	public void enleverA(int pos)
-	{
-		if(isEmpty())
-		{return;}
-		int last = -1;
-		for(int i = 0;i < tab.length;i++)
-		{
-			if(tab[i] != null)
-			{last = i;}
-		}
 
     public void remplirTab(char[][] ch)
     {
@@ -411,171 +408,100 @@ public class TabAligne3
         }
     }
 
-	private void enleverA(Aligne3 a)
-	{
-		int last = -1;
-		int pos = -1;
-		for(int i = 0;i < tab.length;i++)
-		{
-			if(tab[i] != null)
-			{last = i;}
-			if(tab[i] == a)
-			{pos = i;}
-		}
-		tab[pos] = tab[last];
-		tab[last] = null;
-	}
 
-	public boolean isEmpty()
-	{
-		for(int i = 0;i < tab.length; i++)
-		{
-			if(tab[i] != null)
-			{return false;} // encore rempli
-		}
-		return true; // vide
-	}
+    public static void afficheTab(int longueur, char[][] ch)
+    {
+        final int longGrid = longueur*2;
+        char x = (char)12288;
+        int t = -1;
 
-	public void viderTab()
-	{
-		for(int i = 0;i < tab.length; i++)
-		{
-			tab[i] = null;
-		}
-	}
+        for(int i = 0; i < longGrid; i++)
+        {
+            if(i%2 == 0)
+            {
+                System.out.print(x);
+            }
+            else
+            {
+                System.out.print((i/2)+1);
+            }
+        }
+        System.out.println();
+        for(int j = 0; j < longGrid+1; j++)
+        {
+            if(j %2 == 0)
+            {
+                for(int c = 0; c < longGrid+1; c++)
+                {
+                    if(j == 0)
+                    {
+                        if(c == 0)
+                        {x = (char)9556;}
+                        else if(c == longGrid)
+                        {x = (char)9559;}
+                        else
+                        {
+                            if(c %2 == 0)
+                            {x = (char)9572;}
+                            else
+                            {x = (char)9552;}
+                        }
+                    }
+                    else if(j == longGrid)
+                    {
+                        if(c == 0)
+                        {x = (char)9562;}
+                        else if(c == longGrid)
+                        {x = (char)9565;}
+                        else
+                        {
+                            if(c %2 == 0)
+                            {x = (char)9575;}
+                            else
+                            {x = (char)9552;}
+                        }
+                    }
+                    else
+                    {
+                        if(c == 0)
+                        {x = (char)9567;}
+                        else if(c == longGrid)
+                        {x = (char)9570;}
+                        else
+                        {
+                            if(c %2 == 0)
+                            {x = (char)9532;}
+                            else
+                            {x = (char)9472;}
+                        }
+                    }
+                    System.out.print(x);
+                }
+            }
+            else
+            {
+                int r = 0;
+                for(int c = 0; c < longGrid+1; c++)
+                {
+                    if(c == 0 || c == longGrid)
+                    {x = (char)9553;}
+                    else
+                    {
+                        if(c%2 == 0)
+                        {x = (char)9474;}
+                        else
+                        {
+                            if(r == 0)
+                            {t++;}
+                            x = ch[t][r];
+                            r++;
+                        }
+                    }
 
-	public void remplirTab(char[][] ch)
-	{
-		for(int i = 0; i < ch.length; i++)
-		{
-			for(int j = 0; j < ch[i].length-2; j++)
-			{
-				if(ch[i][j] != (char)12288 && ch[i][j] == ch[i][j+1] && ch[i][j] == ch[i][j+2])
-				{
-					ajouterA(i, j, (ch[i][j] == Puissance4.j1)?true: false, 1);// i j  = coordonnee extremite gauche
-				}//en ligne
-
-				if(ch[j][i] != (char)12288 && ch[j][i] == ch[j+1][i] && ch[j][i] == ch[j+2][i])
-				{
-					ajouterA(j, i, (ch[j][i] == Puissance4.j1)?true: false, 2);// i j = coordonnee extremite superieure
-				}//en colonne
-			}
-		}
-		for(int i = 0; i < ch.length -2; i++)
-		{
-			for(int j = 0; j < ch[i].length-2 ; j++)
-			{
-				if(ch[i][j] != (char)12288 && ch[i][j] == ch[i+1][j+1] && ch[i][j] == ch[i+2][j+2])
-				{
-					ajouterA(i  , j, (ch[i][j] == Puissance4.j1)?true: false, 3);// i j = coordonnee extremite haut gauche
-				}//en diagonale descendante
-			}
-		}
-		for(int i = 2; i < ch.length; i++)
-		{
-			for(int j = 0; j < ch[i].length-2 ; j++)
-			{
-				if(ch[i][j] != (char)12288 && ch[i][j] == ch[i-1][j+1] && ch[i][j] == ch[i-2][j+2])
-				{
-					ajouterA(i  , j, (ch[i][j] == Puissance4.j1)?true: false, 4);// i j = coordonnee extremite bas gauche
-				}//en diagonale montante
-			}
-		}
-	}
-
-	public static void afficheTab(int longueur, char[][] ch)
-	{
-		final int longGrid = longueur*2;
-		char x = (char)12288;
-		int t = -1;
-
-		for(int i = 0; i < longGrid; i++)
-		{
-			if(i%2 == 0)
-			{
-				System.out.print(x);
-			}
-			else
-			{
-				System.out.print((i/2)+1);
-			}
-		}
-		System.out.println();
-		for(int j = 0; j < longGrid+1; j++)
-		{
-			if(j %2 == 0)
-			{
-				for(int c = 0; c < longGrid+1; c++)
-				{
-					if(j == 0)
-					{
-						if(c == 0)
-						{x = (char)9556;}
-						else if(c == longGrid)
-						{x = (char)9559;}
-						else
-						{
-							if(c %2 == 0)
-							{x = (char)9572;}
-							else
-							{x = (char)9552;}
-						}
-					}
-					else if(j == longGrid)
-					{
-						if(c == 0)
-						{x = (char)9562;}
-						else if(c == longGrid)
-						{x = (char)9565;}
-						else
-						{
-							if(c %2 == 0)
-							{x = (char)9575;}
-							else
-							{x = (char)9552;}
-						}
-					}
-					else
-					{
-						if(c == 0)
-						{x = (char)9567;}
-						else if(c == longGrid)
-						{x = (char)9570;}
-						else
-						{
-							if(c %2 == 0)
-							{x = (char)9532;}
-							else
-							{x = (char)9472;}
-						}
-					}
-					System.out.print(x);
-				}
-			}
-			else
-			{
-				int r = 0;
-				for(int c = 0; c < longGrid+1; c++)
-				{
-					if(c == 0 || c == longGrid)
-					{x = (char)9553;}
-					else
-					{
-						if(c%2 == 0)
-						{x = (char)9474;}
-						else
-						{
-							if(r == 0)
-							{t++;}
-							x = ch[t][r];
-							r++;
-						}
-					}
-
-					System.out.print(x);
-				}
-			}
-			System.out.println();
-		}
-	}
+                    System.out.print(x);
+                }
+            }
+            System.out.println();
+        }
+    }
 }
